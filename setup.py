ca73cb9a58f9d57f3b2816454f44f72040ff5fc2
Alex Ehlke <jehlke@student.umass.edu>
2010-11-09 20:37:11 -0500

    add template dirs, which otherwise would have been ignored due to not being Python modules, to setup

--- setup.py
+++ setup.py
@@ -29,10 +29,21 @@ for dirpath, dirnames, filenames in os.walk(package_dir):
     if "__init__.py" in filenames:
         packages.append(".".join(fullsplit(dirpath)))
 
+template_patterns = [
+    'templates/*.html',
+    'templates/*/*.html',
+    'templates/*/*/*.html',
+]
+
+package_data = dict(
+    (package_name, template_patterns)
+    for package_name in packages
+)
 
 setup(name='django-basic-apps',
     version='0.7',
     description='Django Basic Apps',
     author='Nathan Borror',
     url='http://github.com/nathanborror/django-basic-apps',
-    packages=packages)
+    packages=packages,
+    package_data=package_data)
